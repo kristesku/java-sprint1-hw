ca7546d21072398a1e16131aceb5a62430e75e71
Роман Москвин <moskvin1844@yandex.ru>
2022-05-09 09:25:20 +0300

    Rollback

--- src/Main.java
+++ src/Main.java
@@ -2,110 +2,110 @@ import java.util.Scanner;
 
 public class Main {
 
-  public static void main(String[] args) {
-    int minCommand = 0;
-    int maxCommand = 3;
-    StepTracker stepTracker = new StepTracker();
+    public static void main(String[] args) {
+        int minCommand = 0;
+        int maxCommand = 3;
+        StepTracker stepTracker = new StepTracker();
 
-    System.out.println("\n# StepTracker v0.1\n");
-    while (true) {
-      printMenu();
-      String message = "Введите номер команды (" + minCommand + "-" + maxCommand + "): ";
-      int command = inputInt(message, minCommand, maxCommand);
-      if (command == 0) {
-        System.out.println("\nЗавершение программы. Всего доброго!");
-        return;
-      } else if (command == 1) {
-        addWalkedStepsData(stepTracker);
-      } else if (command == 2) {
-        showMonthStatistics(stepTracker);
-      } else if (command == 3) {
-        setTargetSteps(stepTracker);
-      }
-    }
-  }
-
-  private static void printMenu() {
-    System.out.println("-----------------------------------------------");
-    System.out.println("1 - Ввести количество пройденных шагов за день.");
-    System.out.println("2 - Вывести статистику за месяц.");
-    System.out.println("3 - Изменить целевое количество шагов в день.");
-    System.out.println("0 - Выход.");
-    System.out.println("-----------------------------------------------");
-  }
-
-  private static int inputInt(String message, int minValue, int maxValue) {
-    while (true) {
-      System.out.print(message);
-      Scanner scanner = new Scanner(System.in);
-      try {
-        int inputValue = scanner.nextInt();
-        if ((inputValue >= minValue) & (inputValue <= maxValue)) {
-          return inputValue;
-        } else {
-          System.out.println("Введено некорректное значение. Пожалуйста, повторите ввод.");
+        System.out.println("\n# StepTracker v0.1\n");
+        while (true) {
+            printMenu();
+            String message = "Введите номер команды (" + minCommand + "-" + maxCommand + "): ";
+            int command = inputInt(message, minCommand, maxCommand);
+            if (command == 0) {
+                System.out.println("\nЗавершение программы. Всего доброго!");
+                return;
+            } else if (command == 1) {
+                addWalkedStepsData(stepTracker);
+            } else if (command == 2) {
+                showMonthStatistics(stepTracker);
+            } else if (command == 3) {
+                setTargetSteps(stepTracker);
+            }
         }
-      } catch (java.util.InputMismatchException e) {
-        System.out.println("Введено некорректное значение. Пожалуйста, повторите ввод.");
-      }
     }
-  }
 
-  private static void addWalkedStepsData(StepTracker stepTracker) {
-    System.out.println("\n## Ввод количества пройденных шагов за день.");
+    private static void printMenu() {
+        System.out.println("-----------------------------------------------");
+        System.out.println("1 - Ввести количество пройденных шагов за день.");
+        System.out.println("2 - Вывести статистику за месяц.");
+        System.out.println("3 - Изменить целевое количество шагов в день.");
+        System.out.println("0 - Выход.");
+        System.out.println("-----------------------------------------------");
+    }
 
-    int yearIndex = getYearIndex();
-    int monthIndex = getMonthIndex();
+    private static int inputInt(String message, int minValue, int maxValue) {
+        while (true) {
+            System.out.print(message);
+            Scanner scanner = new Scanner(System.in);
+            try {
+                int inputValue = scanner.nextInt();
+                if ((inputValue >= minValue) & (inputValue <= maxValue)) {
+                    return inputValue;
+                } else {
+                    System.out.println("Введено некорректное значение. Пожалуйста, повторите ввод.");
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Введено некорректное значение. Пожалуйста, повторите ввод.");
+            }
+        }
+    }
 
-    int minDay = 1;
-    int daysInMonth = stepTracker.getNumberOfDaysInMonth(yearIndex, monthIndex);
-    String message = "Введите день (" + minDay + "-" + daysInMonth + "): ";
-    int dayIndex = inputInt(message, minDay, daysInMonth) - 1;
+    private static void addWalkedStepsData(StepTracker stepTracker) {
+        System.out.println("\n## Ввод количества пройденных шагов за день.");
 
-    int minSteps = 1;
-    int maxSteps = Integer.MAX_VALUE;
-    int stepsWalked = inputInt("Введите количество пройденных шагов: ", minSteps, maxSteps);
+        int yearIndex = getYearIndex();
+        int monthIndex = getMonthIndex();
 
-    stepTracker.addWalkedStepsData(yearIndex, monthIndex, dayIndex, daysInMonth, stepsWalked);
+        int minDay = 1;
+        int daysInMonth = stepTracker.getNumberOfDaysInMonth(yearIndex, monthIndex);
+        String message = "Введите день (" + minDay + "-" + daysInMonth + "): ";
+        int dayIndex = inputInt(message, minDay, daysInMonth) - 1;
 
-    String formattedDate = stepTracker.getFormattedDate(yearIndex, monthIndex, dayIndex);
-    System.out.println("\nЗа " + formattedDate + " пройдено " + stepsWalked + " шагов.\n");
-  }
+        int minSteps = 1;
+        int maxSteps = Integer.MAX_VALUE;
+        int stepsWalked = inputInt("Введите количество пройденных шагов: ", minSteps, maxSteps);
 
-  private static int getMonthIndex() {
-    int minMonth = 1;
-    int maxMonth = 12;
-    String message = "Введите месяц (" + minMonth + "-" + maxMonth + "): ";
-    return inputInt(message, minMonth, maxMonth) - 1;
-  }
+        stepTracker.addWalkedStepsData(yearIndex, monthIndex, dayIndex, daysInMonth, stepsWalked);
 
-  private static int getYearIndex() {
-    int minYear = 0;
-    int maxYear = 3000;
-    String message = "Введите год (" + minYear + "-" + maxYear + "): ";
-    return inputInt(message, minYear, maxYear) - 1;
-  }
+        String formattedDate = stepTracker.getFormattedDate(yearIndex, monthIndex, dayIndex);
+        System.out.println("\nЗа " + formattedDate + " пройдено " + stepsWalked + " шагов.\n");
+    }
 
-  private static void showMonthStatistics(StepTracker stepTracker) {
-    System.out.println("\n## Получение статистики за месяц по пройденным шагам.");
+    private static int getMonthIndex() {
+        int minMonth = 1;
+        int maxMonth = 12;
+        String message = "Введите месяц (" + minMonth + "-" + maxMonth + "): ";
+        return inputInt(message, minMonth, maxMonth) - 1;
+    }
 
-    int yearIndex = getYearIndex();
-    int monthIndex = getMonthIndex();
+    private static int getYearIndex() {
+        int minYear = 0;
+        int maxYear = 3000;
+        String message = "Введите год (" + minYear + "-" + maxYear + "): ";
+        return inputInt(message, minYear, maxYear) - 1;
+    }
 
-    String stepStatistics = stepTracker.getStepStatistics(yearIndex, monthIndex);
-    System.out.println("\n" + stepStatistics + "\n");
-  }
+    private static void showMonthStatistics(StepTracker stepTracker) {
+        System.out.println("\n## Получение статистики за месяц по пройденным шагам.");
 
-  private static void setTargetSteps(StepTracker stepTracker) {
-    int minTarget = 1;
-    int maxTarget = Integer.MAX_VALUE;
+        int yearIndex = getYearIndex();
+        int monthIndex = getMonthIndex();
 
-    System.out.println("\n## Получение статистики за месяц по пройденным шагам.");
-    String message =
-        "Введите новое целевое количество шагов в день (" + minTarget + "-" + maxTarget + "): ";
-    int newTarget = inputInt(message, minTarget, maxTarget);
+        String stepStatistics = stepTracker.getStepStatistics(yearIndex, monthIndex);
+        System.out.println("\n" + stepStatistics + "\n");
+    }
 
-    stepTracker.setTargetSteps(newTarget);
-    System.out.println("\nНовая цель — " + newTarget + " шагов в день\n");
-  }
+    private static void setTargetSteps(StepTracker stepTracker) {
+        int minTarget = 1;
+        int maxTarget = Integer.MAX_VALUE;
+
+        System.out.println("\n## Получение статистики за месяц по пройденным шагам.");
+        String message =
+                "Введите новое целевое количество шагов в день (" + minTarget + "-" + maxTarget + "): ";
+        int newTarget = inputInt(message, minTarget, maxTarget);
+
+        stepTracker.setTargetSteps(newTarget);
+        System.out.println("\nНовая цель — " + newTarget + " шагов в день\n");
+    }
 }
